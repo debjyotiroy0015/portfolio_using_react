e3ecd75ea67c3cd897f811e17796f1ad0196623d
debjyotiroy <debjyotiry@gmail.com>
2024-03-31 16:43:16 +0530

    "1st version of navbar"

---
+++ portfolio_using_react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "../src/App.css";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ portfolio_using_react/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "../../src/assets/css/navbar.css";
+import { FaReact } from "react-icons/fa";
+import { CiMenuBurger } from "react-icons/ci";
+import { RxCross1 } from "react-icons/rx";
+const Navbar = () => {
+  const [menuBar, setMenuBar] = useState(true);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <FaReact className="logo-icon" />
+      </div>
+      <div className="menu-option">
+        {menuBar ? (
+          <CiMenuBurger
+            className="menu-bar"
+            onClick={() => {
+              setMenuBar(false);
+            }}
+          />
+        ) : (
+          <RxCross1
+            className="menu-bar"
+            onClick={() => {
+              setMenuBar(true);
+            }}
+          />
+        )}
+
+        <ul className={menuBar ? "menu-option-list" : "menu-option-list-sm"}>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Skills & Experience</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
