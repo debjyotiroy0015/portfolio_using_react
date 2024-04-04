d727116fd8a4dbef12fdce7d621eb1f6c1b79bba
debjyotiroy <debjyotiry@gmail.com>
2024-04-05 01:07:47 +0530

    "add locomotive and fixed fontsize for mobile view"

--- portfolio_using_react/src/App.jsx
+++ portfolio_using_react/src/App.jsx
@@ -3,7 +3,10 @@ import Navbar from "./components/Navbar";
 import "../src/App.css";
 import HeroSection from "./components/HeroSection";
 import Skills from "./components/Skills";
+import LocomotiveScroll from "locomotive-scroll";
+
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
       <Navbar />
